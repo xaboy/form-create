beed8e3e0beeae2d7ed81b9fb5a852f4dca1f174
孙力强 <xaboy@mpo.local>
2018-08-23 09:12:14 +0800

    增加build,dev命令

---
+++ build/webpack.dev.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app:path.join(__dirname,'../src','index.js')
+    },
+    output: {//输出文件
+        filename: 'dist/form-create.js',
+        libraryTarget:'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.BannerPlugin('form-create v1.3 | github https://github.com/xaboy/form-create | author xaboy'),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../demo/index.html'),
+            filename: path.resolve(__dirname, '../demo/index.html'),
+            inject: true
+        })
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    }
+};
---
+++ build/webpack.dist.prod.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+module.exports = {
+    entry: __dirname + '/../src/index.js',
+    output: {//输出文件
+        filename: 'form-create.min.js',
+        path: __dirname + '/../dist',
+        libraryTarget:'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output:{
+                comments:false,
+                beautify: false
+            }
+        }),
+        new webpack.BannerPlugin('form-create v1.3 | github https://github.com/xaboy/form-create | author xaboy')
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.js'
+        }
+    }
+};
