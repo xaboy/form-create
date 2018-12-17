c01dd5d5d24f464131160b30f6df10480c4117d1
xaboy <xaboy2005@qq.com>
2018-12-17 22:34:09 +0800

    优化 upload,frame 组件样式

--- src/core/common.js
+++ src/core/common.js
@@ -116,14 +116,15 @@ export function getConfig() {
     };
 };
 
-export const formCreateStyle = '.form-create{padding:25px;} .fc-upload-btn,.fc-files{display: inline-block;width: 58px;height: 58px;text-align: center;line-height: 58px;border: 1px solid transparent;border-radius: 4px;overflow: hidden;background: #fff;position: relative;box-shadow: 0 1px 1px rgba(0,0,0,.2);margin-right: 4px;box-sizing: border-box;}.__fc_h{display:none;}.__fc_v{visibility:hidden;}' +
+export const formCreateStyle = '.form-create{padding:25px;} .fc-upload-btn,.fc-files{display: inline-block;width: 58px;height: 58px;text-align: center;line-height: 58px;border: 1px solid #c0ccda;border-radius: 4px;overflow: hidden;background: #fff;position: relative;box-shadow: 2px 2px 5px rgba(0,0,0,.1);margin-right: 4px;box-sizing: border-box;}.__fc_h{display:none;}.__fc_v{visibility:hidden;}' +
     ' .fc-files>.ivu-icon{vertical-align: middle;}' +
     '.fc-files img{width:100%;height:100%;display:inline-block;vertical-align: top;}' +
     '.fc-upload .ivu-upload{display: inline-block;}' +
-    '.fc-upload-btn{border: 1px dashed #dddee1;}' +
-    '.fc-upload .fc-upload-cover{ display: none; position: absolute; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0,0,0,.6); }' +
+    '.fc-upload-btn{border: 1px dashed #c0ccda;}' +
+    '.fc-upload-btn>ivu-icon{vertical-align:sub;}' +
+    '.fc-upload .fc-upload-cover{opacity: 0; position: absolute; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0,0,0,.6); transition: opacity .3s;}' +
     '.fc-upload .fc-upload-cover i{ color: #fff; font-size: 20px; cursor: pointer; margin: 0 2px; }' +
-    '.fc-files:hover .fc-upload-cover{ display: block; }' +
+    '.fc-files:hover .fc-upload-cover{opacity: 1; }' +
     '.fc-upload .ivu-upload-list-file{ display: inline-block;float: left; }' +
     '.fc-upload .ivu-upload-list{ position: absolute;left: 0; }' +
     '.fc-spin-icon-load{animation: ani-fc-spin 1s linear infinite;} @-webkit-keyframes ani-fc-spin{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}50%{-webkit-transform:rotate(180deg);transform:rotate(180deg)}to{-webkit-transform:rotate(1turn);transform:rotate(1turn)}}@keyframes ani-fc-spin{0%{-webkit-transform:rotate(0deg);transform:rotate(0deg)}50%{-webkit-transform:rotate(180deg);transform:rotate(180deg)}to{-webkit-transform:rotate(1turn);transform:rotate(1turn)}}';
