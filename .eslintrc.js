79e1e522a25118df40114e3490a2c0ac72382aa1
xaboy <309058341@qq.com>
2019-05-14 19:46:12 +0800

    配置 eslint+prettier

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:vue/essential'
+    ],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'vue','prettier'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
