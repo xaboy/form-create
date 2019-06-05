b09e9cc4334ab9c9e59ab2edf119a2048b55f7bd
xaboy <309058341@qq.com>
2019-06-05 17:51:58 +0800

    删除多余文件

--- packages/core/src2/utils.js
+++
@@ -1,9 +0,0 @@
-import {isFunction} from '@form-create/utils';
-
-
-export function getRule(rule) {
-    if (isFunction(rule.getRule))
-        return rule.getRule();
-    else
-        return rule;
-}
