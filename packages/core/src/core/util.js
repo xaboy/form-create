af4b500ba791a557117fb5fb0991586bcdbe525a
xaboy <309058341@qq.com>
2019-08-25 19:38:50 +0800

    fix(toJson): 修复函数转 json

--- packages/core/src/core/util.js
+++ packages/core/src/core/util.js
@@ -18,7 +18,7 @@ export function toJson(obj) {
         if (val.__emit)
             return undefined;
 
-        return val;
+        return '' + val;
     });
 }
 
@@ -26,7 +26,12 @@ export function toJson(obj) {
 export function parseJson(json) {
     return JSON.parse(json, function (k, v) {
         if (v.indexOf && v.indexOf('function') > -1) {
-            return eval('(function(){return ' + v + ' })()')
+            try {
+                return eval('(function(){return ' + v + ' })()')
+            } catch (e) {
+                console.error(`[form-create]解析失败:${v}`);
+                return undefined;
+            }
         }
         return v;
     });
