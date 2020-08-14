a3737e40a86fe5ee2d154cb3376c1b26595ed390
xaboy <xaboy2005@qq.com>
2020-08-14 11:33:04 +0800

    fix: 避免 key 重复

--- components/element-ui/select/src/component.jsx
+++ components/element-ui/select/src/component.jsx
@@ -13,7 +13,7 @@ export default {
     render(h, ctx) {
         return <ElSelect {...ctx.data}>{ctx.props.options.map((props, index) => {
             return <ElOption {...{props}}
-                key={index + props.value}>
+                key={'' + index + props.value}>
                 {props.slot ? <template slot={props.slotName || 'default'}>{toSlot(props.slot, h)}</template> : null}
             </ElOption>
         }).concat(ctx.chlidren)}</ElSelect>;
