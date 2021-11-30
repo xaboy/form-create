994b7f3528dcdc869bef050b1afeacdf615de8b7
xaboy <xaboy2005@qq.com>
2021-11-30 10:54:56 +0800

    feat(editor): 增加 config 参数

--- components/common/wangeditor/src/component.jsx
+++ components/common/wangeditor/src/component.jsx
@@ -4,12 +4,23 @@ const NAME = 'fcEditor';
 
 let uni = 1;
 
+const _extends = Object.assign || function (a) {
+    for (let b, c = 1; c < arguments.length; c++) {
+        for (let d in b = arguments[c], b) {
+            Object.prototype.hasOwnProperty.call(b, d) && (a[d] = b[d]);
+        }
+    }
+
+    return a;
+}
+
 export default {
     name: NAME,
     props: {
         value: String,
         init: Function,
         disabled: Boolean,
+        config: Object,
     },
     data() {
         return {
@@ -28,7 +39,7 @@ export default {
         }
     },
     methods: {
-        enable(){
+        enable() {
             this.disabled ? this.editor.disable() : this.editor.enable();
         },
         result() {
@@ -36,12 +47,15 @@ export default {
         }
     },
     mounted() {
-        this.editor = new WangEditor(`#editor${this.uni}`);
-        this.editor.config.zIndex = 2;
-        this.init && this.init(this.editor);
-        this.editor.create();
-        this.enable();
-        this.editor.txt.html(this.value);
+        this.$nextTick(() => {
+            this.editor = new WangEditor(`#editor${this.uni}`);
+            this.editor.config.zIndex = 2;
+            this.config && _extends(this.editor.config, this.config);
+            this.init && this.init(this.editor);
+            this.editor.create();
+            this.enable();
+            this.editor.txt.html(this.value);
+        });
     },
     render() {
         return <div on-input={this.result} id={`editor${this.uni}`} style="line-height: normal;"/>
