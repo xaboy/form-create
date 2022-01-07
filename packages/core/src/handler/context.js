7a528ab67f78f5ae1c88e25f014666d1b9858730
xaboy <xaboy2005@qq.com>
2022-01-07 19:09:38 +0800

    cli: fix

--- packages/core/src/handler/context.js
+++ packages/core/src/handler/context.js
@@ -1,7 +1,7 @@
 import extend from '@form-create/utils/lib/extend';
 import toCase from '@form-create/utils/lib/tocase';
 import BaseParser from '../factory/parser';
-import {$del} from '@form-create/utils/lib';
+import {$del} from '@form-create/utils/lib/modify';
 import is from '@form-create/utils/lib/type';
 import {invoke} from '../frame/util';
 
