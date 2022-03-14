70e026b0a1758002223107d8923f96848e6bc7ae
xaboy <>
2022-03-14 18:32:35 +0800

    fix(#466): 修复 AsyncFunction 方法未执行问题

--- packages/utils/lib/type.js
+++ packages/utils/lib/type.js
@@ -10,10 +10,29 @@ const is = {
     },
     trueArray(data) {
         return Array.isArray(data) && data.length > 0;
+    },
+    Function(v) {
+        const type = this.getType(v);
+        return type === 'Function' || type === 'AsyncFunction';
+    },
+    getType(v) {
+        const str = Object.prototype.toString.call(v)
+        return /^\[object (.*)\]$/.exec(str)[1]
+    },
+    empty(value) {
+        if (value === undefined || value === null) {
+            return true;
+        }
+
+        if (Array.isArray(value) && Array.isArray(value) && !value.length) {
+            return true;
+        }
+
+        return typeof value === 'string' && !value;
     }
 };
 
-['Date', 'Object', 'Function', 'String', 'Boolean', 'Array', 'Number'].forEach(t => {
+['Date', 'Object', 'String', 'Boolean', 'Array', 'Number'].forEach(t => {
     is[t] = function (arg) {
         return is.type(arg, t);
     }
