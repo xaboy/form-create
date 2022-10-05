4dd8752e60ae5302fc0499a34f3a57730cc8747d
xaboy <>
2022-10-05 19:58:06 +0800

    refactor: 优化 uniqueId

--- packages/utils/lib/unique.js
+++ packages/utils/lib/unique.js
@@ -1,5 +1,5 @@
 let id = 0;
 
 export default function uniqueId() {
-    return Math.random().toString(36).substr(3, 3) + Number(`${Date.now()}${++id}`).toString(36);
+    return 'F' + Math.random().toString(36).substr(3, 3) + Number(`${Date.now()}${++id}`).toString(36);
 }
