f6c287647cdfb7b4f3f304a4748586cc98f46631
xaboy <>
2023-04-17 15:14:23 +0800

    fix: 修复tree组件渲染

--- packages/arco-design/src/core/alias.js
+++ packages/arco-design/src/core/alias.js
@@ -25,7 +25,7 @@ export default {
     frame: 'fcFrame',
     col: PRE + 'Col',
     row: PRE + 'Row',
-    tree: PRE + 'TreeSelect',
+    tree: PRE + 'Tree',
     autoComplete: PRE + 'AutoComplete',
     group: 'fcGroup',
     subForm: 'fcSubForm',
