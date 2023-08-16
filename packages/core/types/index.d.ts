1859e5036cc55605c52cde7e85cf790d9dd49f35
xaboy <>
2023-08-16 14:44:14 +0800

    docs: update ts

--- packages/core/types/index.d.ts
+++ packages/core/types/index.d.ts
@@ -551,7 +551,7 @@ export interface FetchOption {
 export interface FetchEffectOption {
     action: String;
     to?: String;
-    parse?: (body: any, rule: Object, api: Object) => any;
+    parse?: String | ((body: any, rule: Object, api: Object) => any);
     method?: String;
     data?: Object;
     dataType?: 'json';
