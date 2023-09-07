6844b497524297466195db765cb503dd240061f9
xaboy <xaboy2005@qq.com>
2023-09-07 17:46:52 +0800

    feat: select组件支持optionGroup

--- components/element-ui/select/src/component.jsx
+++ components/element-ui/select/src/component.jsx
@@ -1,4 +1,4 @@
-import is from '@form-create/utils/lib/type';
+import is, {hasProperty} from '@form-create/utils/lib/type';
 
 const NAME = 'fcSelect';
 export default {
@@ -11,17 +11,29 @@ export default {
         },
     },
     render(h, ctx) {
-        const options = ctx.props.formCreateInject.options;
-        return <ElSelect {...ctx.data} ref="el">{(Array.isArray(options) ? options : []).map((props, index) => {
+        const makeOption = (props, index) => {
             const slot = props.slot;
             return <ElOption props={props}
                 key={'' + index + '-' + props.value}>
                 {slot ? <template
                     slot={props.slotName || 'default'}>{is.Function(slot) ? props.slot(h) : slot}</template> : null}
-            </ElOption>
+            </ElOption>;
+        }
+        const makeOptionGroup = (props, index) => {
+            return <ElOptionGroup label={props.label}
+                key={'' + index + '-' + props.label}>
+                {is.trueArray(props.options) && props.options.map((v, index) => {
+                    return makeOption(v, index);
+                })}
+            </ElOptionGroup>;
+        }
+
+        const options = ctx.props.formCreateInject.options;
+        return <ElSelect {...ctx.data} ref="el">{(Array.isArray(options) ? options : []).map((props, index) => {
+            return hasProperty(props || '', 'options') ? makeOptionGroup(props, index) : makeOption(props, index);
         })}{ctx.children}</ElSelect>;
     },
-    mounted(){
+    mounted() {
         this.$emit('fc.el', this.$refs.el);
     }
 }
--- components/iview/select/src/component.jsx
+++ components/iview/select/src/component.jsx
@@ -1,4 +1,4 @@
-import is from '@form-create/utils/lib/type';
+import is, {hasProperty} from '@form-create/utils/lib/type';
 
 const NAME = 'fcSelect';
 export default {
@@ -11,14 +11,26 @@ export default {
         },
     },
     render(h, ctx) {
-        const options = ctx.props.formCreateInject.options;
-        return <Select {...ctx.data} ref="el">{(Array.isArray(options) ? options : []).map((props, index) => {
+        const makeOption = (props, index) => {
             const slot = props.slot;
             return <Option props={props}
                 key={'' + index + '-' + props.value}>
                 {slot ? <template
                     slot={props.slotName || 'default'}>{is.Function(slot) ? props.slot(h) : slot}</template> : null}
-            </Option>
+            </Option>;
+        }
+        const makeOptionGroup = (props, index) => {
+            return <OptionGroup label={props.label}
+                key={'' + index + '-' + props.label}>
+                {is.trueArray(props.options) && props.options.map((v, index) => {
+                    return makeOption(v, index);
+                })}
+            </OptionGroup>;
+        }
+
+        const options = ctx.props.formCreateInject.options;
+        return <Select {...ctx.data} ref="el">{(Array.isArray(options) ? options : []).map((props, index) => {
+            return hasProperty(props || '', 'options') ? makeOptionGroup(props, index) : makeOption(props, index);
         })}{ctx.children}</Select>;
     },
     mounted() {
