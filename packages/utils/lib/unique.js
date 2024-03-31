bcdfa0657224e96f4ed8be871456d1d7a8347f30
xaboy <xaboy2005@qq.com>
2024-03-31 16:42:01 +0800

    refactor: 优化唯一值生成

--- packages/utils/lib/unique.js
+++ packages/utils/lib/unique.js
@@ -1,5 +1,6 @@
 let id = 0;
 
 export default function uniqueId() {
-    return 'F' + Math.random().toString(36).substr(3, 3) + Number(`${Date.now()}${++id}`).toString(36);
+    const num = 370 + (++id);
+    return 'F' + Math.random().toString(36).substr(3, 3) + Number(`${Date.now()}`).toString(36) + num.toString(36) + 'c';
 }
