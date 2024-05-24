677053c3b096116762fcc9f6aec4f53582f147fb
xaboy <xaboy2005@qq.com>
2024-05-24 12:25:04 +0800

    refactor: 修改validate注入方式

--- packages/core/src/factory/context.js
+++ packages/core/src/factory/context.js
@@ -1,7 +1,7 @@
 import unique from '@form-create/utils/lib/unique';
 import toCase from '@form-create/utils/lib/tocase';
 import extend from '@form-create/utils/lib/extend';
-import {enumerable, invoke, mergeRule, setPrototypeOf} from '../frame/util';
+import {enumerable, invoke, mergeRule} from '../frame/util';
 import {deepCopy} from '@form-create/utils/lib/deepextend';
 import {markRaw, reactive} from 'vue';
 import is from '@form-create/utils/lib/type';
@@ -168,14 +168,20 @@ extend(RuleContext.prototype, {
     },
     injectValidate() {
         return toArray(this.prop.validate).map(item => {
-            return setPrototypeOf({...item}, {
-                inject: {
-                    id: this.id,
-                    field: this.field,
-                    rule: this.rule,
-                    api: this.$handle.api,
+            if (is.Function(item.validator)) {
+                const temp = {...item};
+                const that = this;
+                temp.validator = function (...args) {
+                    return item.validator.call({
+                        that: this,
+                        id: that.id,
+                        field: that.field,
+                        rule: that.rule,
+                        api: that.$handle.api,
+                    }, ...args)
                 }
-            });
+            }
+            return item;
         });
     },
     check(handle) {
