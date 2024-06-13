fdd936279016b39db1cd41e1069ec9a777c1d222
xaboy <xaboy2005@qq.com>
2024-06-13 17:24:44 +0800

    refactor: 修改appendValue默认值为true

--- packages/core/src/handler/input.js
+++ packages/core/src/handler/input.js
@@ -58,7 +58,7 @@ export default function useInput(Handler) {
                 handle.configurable = true;
                 initial[field] = handle;
                 return initial;
-            }, this.options.appendValue ? Object.keys(this.appendData).reduce((initial, field) => {
+            }, this.options.appendValue !== false ? Object.keys(this.appendData).reduce((initial, field) => {
                 initial[field] = {
                     enumerable: true,
                     configurable: true,
@@ -113,7 +113,7 @@ export default function useInput(Handler) {
             if (this.deferSyncFn) {
                 return this.deferSyncFn.sync = true;
             }
-            this.vm._updateValue({...(this.options.appendValue ? this.appendData : {}), ...this.form});
+            this.vm._updateValue({...(this.options.appendValue !== false ? this.appendData : {}), ...this.form});
         },
         isChange(ctx, value) {
             return JSON.stringify(ctx.rule.value, strFn) !== JSON.stringify(value === null ? undefined : value, strFn);
