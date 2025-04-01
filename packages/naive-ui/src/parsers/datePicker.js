3084b22d94ef5fae55684cf855b34d364f33aa7f
anyJohn <33611794+anyJohn@users.noreply.github.com>
2025-04-01 16:45:09 +0800

    fix: Set a default format for naive-ui datePicker

--- packages/naive-ui/src/parsers/datePicker.js
+++ packages/naive-ui/src/parsers/datePicker.js
@@ -1,5 +1,17 @@
 import {creatorFactory} from '@form-create/core/src/index';
 
+const DEFAULT_FORMATS = {
+    date: 'yyyy-MM-dd',
+    month: 'yyyy-MM',
+    week: 'yyyy-wo',
+    datetime: 'yyyy-MM-dd HH:mm:ss',
+    timerange: 'HH:mm:ss',
+    daterange: 'yyyy-MM-dd',
+    monthrange: 'yyyy-MM',
+    datetimerange: 'yyyy-MM-dd HH:mm:ss',
+    year: 'yyyy'
+};
+
 const name = 'datePicker';
 
 export default {
@@ -10,5 +22,11 @@ export default {
             initial[type] = creatorFactory(name, {type: type.toLowerCase()});
             return initial
         }, {});
-    }())
+    }()),
+    mergeProp(ctx) {
+        const props = ctx.prop.props
+        if(!props.valueFormat) {
+            props.format = DEFAULT_FORMATS[props.type] || DEFAULT_FORMATS['date'];
+        }
+    }
 }
