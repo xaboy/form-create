b4c4e8c5f80d155c415226771e1941c1191e4957
xaboy <xaboy2005@qq.com>
2026-01-06 20:42:41 +0800

    修复 el-select 组件报错问题 #815

---
+++ components/element-ui/select/src/component.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-select
+      v-bind="$attrs"
+      :model-value="value"
+      @update:model-value="handleUpdate"
+      ref="el"
+  >
+    <template v-for="(props, index) in options" :key="getKey(props, index)">
+      <el-option-group
+          v-if="hasProperty(props, 'options')"
+          :label="props.label"
+          :key="`${index}-${props.label}`"
+      >
+        <el-option
+            v-for="(option, optIndex) in (is.trueArray(props.options) ? props.options : [])"
+            :key="`${optIndex}-${option.value}`"
+            v-bind="option"
+        />
+      </el-option-group>
+      <el-option
+          v-else
+          :key="`${index}-${props.value}`"
+          v-bind="props"
+      />
+    </template>
+    <slot/>
+  </el-select>
+</template>
+
+<script>
+import {computed, defineComponent, toRef} from 'vue';
+import is, {hasProperty} from '@form-create/utils/lib/type';
+
+const NAME = 'fcSelect';
+
+export default defineComponent({
+  name: NAME,
+  inheritAttrs: false,
+  props: {
+    formCreateInject: Object,
+    modelValue: {
+      type: [Array, String, Number, Boolean, Object],
+      default: undefined
+    },
+    type: String,
+  },
+  emits: ['update:modelValue', 'fc.el'],
+  setup(props, {emit}) {
+    const options = toRef(props.formCreateInject, 'options', []);
+    const value = toRef(props, 'modelValue');
+    const _options = computed(() => {
+      return Array.isArray(options.value) ? options.value : [];
+    });
+
+    const handleUpdate = (v) => {
+      emit('update:modelValue', v);
+    };
+
+    const getKey = (props, index) => {
+      if (hasProperty(props, 'options')) {
+        return `${index}-${props.label}`;
+      }
+      return `${index}-${props.value}`;
+    };
+
+    return {
+      options: _options,
+      value,
+      handleUpdate,
+      getKey,
+      hasProperty,
+      is
+    };
+  },
+  mounted() {
+    this.$emit('fc.el', this.$refs.el);
+  }
+});
+</script>
+
--- components/element-ui/select/src/index.js
+++ components/element-ui/select/src/index.js
@@ -1,3 +1,3 @@
-import Select from './component';
+import Select from './component.vue';
 
 export default Select;
