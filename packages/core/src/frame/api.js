50e2495208a55e835a6e823ba91f7222d5ce4556
xaboy <xaboy2005@qq.com>
2026-04-20 16:06:34 +0800

    优化 api.fetch 方法

--- packages/core/src/frame/api.js
+++ packages/core/src/frame/api.js
@@ -609,9 +609,9 @@ export default function Api(h) {
         getCurrentFormRule() {
             return h.vm.getGroupInject()?.rule;
         },
-        fetch(opt) {
+        fetch(_opt) {
             return new Promise((resolve, reject) => {
-                opt = deepCopy(opt);
+                let opt = {..._opt};
                 opt = h.loadFetchVar(opt);
                 const fail = (e) => {
                     invoke(() => opt.onError && opt.onError(e));
@@ -631,7 +631,7 @@ export default function Api(h) {
         },
         watchFetch(opt, callback, error, beforeFetch) {
             return h.fc.watchLoadData((get, change) => {
-                let _opt = deepCopy(opt);
+                let _opt = {...opt};
                 _opt = h.loadFetchVar(_opt, get);
                 const fail = (e) => {
                     invoke(() => _opt.onError && _opt.onError(e));
